a508add2d47fbad41ac1abfa4d275ec5ed10a331
doge15567 <41397214+doge15567@users.noreply.github.com>
2024-03-24 22:07:30 -0500

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,80 @@
+﻿using MelonLoader;
+using BoneLib;
+using System.Runtime.InteropServices;
+using SLZ.Bonelab;
+using System;
+using SLZ.Marrow.Warehouse;
+using Harmony;
+using System.Collections.Generic;
+using System.Linq;
+using BoneLib.BoneMenu.Elements;
+using BoneLib.BoneMenu;
+using UnityEngine;
+
+namespace MoreItemsInDevTools
+{
+    internal partial class Main : MelonMod
+    {
+        
+        internal const string Name = "More Items in Dev Tools";
+        internal const string Description = "Adds more items to list of items spawned by dev tools cheat";
+        internal const string Author = "doge15567";
+        internal const string Company = "";
+        internal const string Version = "1.0.0";
+        internal const string DownloadLink = "https://thunderstore.io/c/bonelab/p/doge15567/MoreItemsInDevTools/";
+        internal static MelonLogger.Instance MelonLog;
+        private static MenuCategory _mainCategory;
+        private static FunctionElement GenerateButtonBM;
+        private static FunctionElement Preset1;
+        private static FunctionElement Preset2;
+        private static FunctionElement Preset3;
+        private static FunctionElement Preset4;
+        private static FunctionElement Preset5;
+        public override void OnInitializeMelon()
+        {
+            MelonLog = LoggerInstance;
+            MelonLog.Msg("Initalised Mod");
+            Prefs.SetupMelonPrefs();
+
+            _mainCategory = MenuManager.CreateCategory("MoreItemsInDevTools", "#f6f6f6");
+            GenerateButtonBM = _mainCategory.CreateFunctionElement("Add Items", Color.cyan, () => { SetCheatMenuItems(Prefs.Barcodes.Value); });
+            Preset1 = _mainCategory.CreateFunctionElement(Prefs.Preset1Name.Value, Color.white, () => { SetCheatMenuItems(Prefs.Preset1.Value); });
+            Preset2 = _mainCategory.CreateFunctionElement(Prefs.Preset2Name.Value, Color.white, () => { SetCheatMenuItems(Prefs.Preset2.Value); });
+            Preset3 = _mainCategory.CreateFunctionElement(Prefs.Preset3Name.Value, Color.white, () => { SetCheatMenuItems(Prefs.Preset3.Value); });
+            Preset4 = _mainCategory.CreateFunctionElement(Prefs.Preset4Name.Value, Color.white, () => { SetCheatMenuItems(Prefs.Preset4.Value); });
+            Preset5 = _mainCategory.CreateFunctionElement(Prefs.Preset5Name.Value, Color.white, () => { SetCheatMenuItems(Prefs.Preset5.Value); });
+
+
+
+
+
+            BoneLib.Hooking.OnLevelInitialized += AddItemsHook;
+        }
+
+        public static void AddItemsHook(LevelInfo info) { SetCheatMenuItems(Prefs.Barcodes.Value); }
+        public static void SetCheatMenuItems(string[] BarcodeStrArray)
+        {
+            MelonLog.Msg("AITCM Called");
+            var playerCheatMenu = Player.rigManager.GetComponent<CheatTool>();
+
+            List<SpawnableCrateReference> newCrateList = new List<SpawnableCrateReference>();
+            foreach (var crateCode in BarcodeStrArray)
+            {
+                MelonLog.Msg("Adding Barcode " + crateCode + " to Array");
+                newCrateList.Add(new SpawnableCrateReference
+                {
+                    _barcode = AssetWarehouse.Instance.GetCrate<GameObjectCrate>(crateCode)._barcode
+                });
+            }
+            playerCheatMenu.crates = newCrateList.ToArray(); // Convert List to Array if necessary
+            MelonLog.Msg("AITCM Ended");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using MoreItemsInDevTools;
+using MelonLoader;
+
+[assembly: AssemblyTitle(MoreItemsInDevTools.Main.Description)]
+[assembly: AssemblyDescription(MoreItemsInDevTools.Main.Description)]
+[assembly: AssemblyCompany(MoreItemsInDevTools.Main.Company)]
+[assembly: AssemblyProduct(MoreItemsInDevTools.Main.Name)]
+[assembly: AssemblyCopyright("Developed by " + MoreItemsInDevTools.Main.Author)]
+[assembly: AssemblyTrademark(MoreItemsInDevTools.Main.Company)]
+[assembly: AssemblyVersion(MoreItemsInDevTools.Main.Version)]
+[assembly: AssemblyFileVersion(MoreItemsInDevTools.Main.Version)]
+[assembly: MelonInfo(typeof(MoreItemsInDevTools.Main), MoreItemsInDevTools.Main.Name, MoreItemsInDevTools.Main.Version, MoreItemsInDevTools.Main.Author, MoreItemsInDevTools.Main.DownloadLink)]
+[assembly: MelonColor(System.ConsoleColor.White)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
